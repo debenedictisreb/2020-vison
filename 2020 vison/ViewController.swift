57cad2d7ad889fdaa0226486fc82f84e68b58c6c
Rebecca DeBenedictis <rjdebened@verizon.net>
2020-07-29 11:12:48 -0400

    Initial Commit

---
+++ 2020 vison/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2020 vison
+//
+//  Created by Rebecca DeBenedictis on 7/29/20.
+//  Copyright © 2020 Rebecca DeBenedictis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 2020 visonTests/_020_visonTests.swift
@@ -0,0 +1,34 @@
+//
+//  _020_visonTests.swift
+//  2020 visonTests
+//
+//  Created by Rebecca DeBenedictis on 7/29/20.
+//  Copyright © 2020 Rebecca DeBenedictis. All rights reserved.
+//
+
+import XCTest
+@testable import _020_vison
+
+class _020_visonTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
