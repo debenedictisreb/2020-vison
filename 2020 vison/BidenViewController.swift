4b8dca596f8d43089353d1ea29698c04bc8a6144
Rebecca DeBenedictis <rjdebened@verizon.net>
2020-07-30 16:35:15 -0400

    final as of thursday afternoon at 4:35

--- 2020 vison/BidenViewController.swift
+++ 2020 vison/BidenViewController.swift
@@ -9,22 +9,51 @@
 import UIKit
 
 class BidenViewController: UIViewController {
-
+  
+    @IBOutlet weak var bidenlabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func gun(_ sender: UIButton) {
+        bidenviews(choice: 0)
     }
-    */
+    
+    @IBAction func immigration(_ sender: UIButton) {
+        bidenviews(choice: 1)
+    }
+    
+    @IBAction func abortion(_ sender: UIButton) {
+        bidenviews(choice: 2)
+    }
 
+    @IBAction func climate(_ sender: UIButton) {
+        bidenviews(choice: 3)
+    }
+    
+    @IBAction func healthcare(_ sender: UIButton) {
+        bidenviews(choice: 4)
+    }
+    @IBAction func economy(_ sender: UIButton) {
+        bidenviews(choice: 5)
+    }
+    
+    func bidenviews (choice : Int) {
+        if choice == 0 {
+            bidenlabel.text = "Supports several changes to gun policy, such as...Reinstating an assault weapons ban; Setting up a voluntary buyback program to get weapons off the streets; Closing loopholes on background checks"
+        } else if choice == 1 {
+            bidenlabel.text = "Against decriminalizing illegal border crossings but acknowledges the “pain” caused by the three million deportations that happened under the Obama admin. Supports DACA and a path to citizenship for undocumented immigrants and has pledged to undo Trump’s 2017 executive order that restricts travel from several Muslim-majority countries."
+        } else if choice == 2 {
+            bidenlabel.text = " Has a mixed record on the issue, which he’s struggled with as a devout Catholic; In 1982, he voted for a bill that, if passed, would’ve let states override Roe v Wade. He has since said he’d defend Roe v Wade as president; No longer supports the Hyde Amendment(which bars federal funding going toward most abortions)"
+        } else if choice == 3 {
+            bidenlabel.text = "Calls climate change the greatest threat to our security. New infrastructure and clean energy plan(scales up and accelerates investments to respond to the economic crisis and create good jobs)"
+        } else if choice == 4 {
+            bidenlabel.text = "Supports free COVID-19 testing and eliminating “all cost barriers” to preventive care and treatment of the virus. Wants to expand the Affordable Care Act by giving Americans the choice of a public health insurance option; He would give Medicare the power to directly negotiate drug prices. Plans to let consumers get prescription drugs from other countries to create more competition for US drug corporations."
+        } else if choice == 5 {
+            bidenlabel.text = "Said the US needs to meaningfully reduce COVID-19 case numbers before the economy can reopen. Wants to address income inequality and workers’ rights by creating a $15/hr minimum wage and ban non-compete agreements that stop workers from taking jobs with competitors. Wants to reverse Trump’s tax law and give tax credits to lower income families."
+        }
+    }
 }
--- 2020 vison/TrumpViewController.swift
+++ 2020 vison/TrumpViewController.swift
@@ -10,21 +10,45 @@ import UIKit
 
 class TrumpViewController: UIViewController {
 
+    @IBOutlet weak var trumplabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func guntrump(_ sender: UIButton) {
+        trumpviews(choice: 0)
     }
-    */
-
-}
+    
+    @IBAction func immigrationtrump(_ sender: UIButton) {
+        trumpviews(choice: 1)
+    }
+    @IBAction func aborttrump(_ sender: UIButton) {
+        trumpviews(choice: 2)
+    }
+    @IBAction func climatetrump(_ sender: UIButton) {
+        trumpviews(choice: 3)
+    }
+    @IBAction func heathcaretrump(_ sender: UIButton) {
+        trumpviews(choice: 4)
+    }
+    @IBAction func economytrump(_ sender: UIButton) {
+        trumpviews(choice: 5)
+    }
+     func trumpviews (choice : Int) {
+           if choice == 0 {
+               trumplabel.text = "Wants to get states to adopt red flag laws, aka courts can more easily take away guns from potentially dangerous people. In 2018 he banned bump stocks, which can make semi-automatic weapons fire more quickly. He has advocated arming teachers with guns to control school shootings"
+           } else if choice == 1 {
+               trumplabel.text = "Has temporarily suspended approval of green card applications to ensure unemployed Americans will be first in line for jobs as the US economy reopens. He closed the US-Mexico border to all but essential services amid COVID-19. He and his admin have also suspended immigration from multiple majority-Muslim countries. He launched a program to have asylum seekers wait in Mexico while claims are processed. He capped the number of refugees admitted to the US. He is still trying to get more funding for border security."
+           } else if choice == 2 {
+              trumplabel.text = "Against abortions except in three instances: rape, incest, and if the mother’s life is at risk. His administration implemented rules that would make it easier for healthcare workers to refuse to perform abortions and other services if they violate their religious or moral beliefs."
+           } else if choice == 3 {
+              trumplabel.text = "Moved to end the Obama admin’s Clean Power Plan and weakened regulations on auto and methane emissions and water pollution. Plan underway to pull the US out of the Paris climate deal by November."
+           } else if choice == 4 {
+              trumplabel.text = "Signed legislation that makes COVID-19 testing free. He hasn’t repealed and replaced the Affordable Care Act, like he said he would on the. 2016 campaign trail. Pushed to lower prescription drug prices and have more transparency in drug pricing."
+           } else if choice == 5 {
+              trumplabel.text = "Republican tax plan, which was the biggest tax rewrite in decades. During his first term, the unemployment rate dropped to a 50-year low ( that changed with COVID-19). Renegotiated a new trade deal with Canada and Mexico."
+           }
+       }
+   }
