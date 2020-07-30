5d617bda5a6bf8598f6adb9228cf30585fd93801
Emma Kelly <emmagk27@icloud.com>
2020-07-29 22:19:39 -0400

    2nd version

---
+++ 2020 vison/BidenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BidenViewController.swift
+//  2020 vison
+//
+//  Created by Emma Kelly on 7/29/20.
+//  Copyright © 2020 Rebecca DeBenedictis. All rights reserved.
+//
+
+import UIKit
+
+class BidenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
